9e9eeb31877a1adb768fe8d9c61f5ef3d12acab1
Rogier van der Gaag <42837230+RMvanderGaag@users.noreply.github.com>
2024-03-19 16:07:38 +0100

    Added composite pattern to backlog

--- AvansDevOps/Backlog/BacklogComponent.cs
+++ AvansDevOps/Backlog/BacklogComponent.cs
@@ -2,5 +2,14 @@ namespace AvansDevOps;
 
 public abstract class BacklogComponent
 {
+    public abstract bool IsComplete { get; set; }
+    public abstract void Complete();
+    public User? AssignedTo { get; private set; }
+
+    public void AssignTo(User user)
+    {
+        AssignedTo = user;
+        Console.WriteLine($"{GetType().Name} is now assigned to {user.Name}.");
+    }
     
 }
