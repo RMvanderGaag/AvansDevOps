a6dcdd2f62c4db6a01bb361b15b35470fc6bf47e
Rogier van der Gaag <rm.vandergaag@student.avans.nl>
2024-03-24 17:57:14 +0100

    Added git tests

--- AvansDevOps/Project/Project.cs
+++ AvansDevOps/Project/Project.cs
@@ -1,22 +1,37 @@
 namespace AvansDevOps;
 
-public class Project : IProject
+public class Project : IProject, IGit
 {
     public string Name { get; set; }
     private List<Sprint> _sprints = new List<Sprint>();
     private User _productOwner;
     public readonly GitAdapter GitAdapter = new GitAdapter();
     
-    public void Commit(BacklogItem backlogItem)
+    public void Commit(IGitItem backlogItem)
     {
         GitAdapter.Commit(backlogItem);
     }
-    
+
     public void Push()
     {
         GitAdapter.Push();
     }
-        
+    
+    public void Pull()
+    {
+        GitAdapter.Pull();
+    }
+    
+    public void Merge()
+    {
+        GitAdapter.Merge();
+    }
+
+    public void Branch()
+    {
+        GitAdapter.Branch();
+    }
+
     public Project(string name, User productOwner)
     {
         Name = name;
--- Tests/ProjectTest.cs
+++ Tests/ProjectTest.cs
@@ -94,5 +94,82 @@ public class ProjectTest
         // Reset the console output
         Console.SetOut(new StreamWriter(Console.OpenStandardOutput()));
     }
+    
+    [Fact]
+    public void ProjectCanPull()
+    {
+        var Project = new Project("Project 1",
+            new User("John Doe", "j.doe@mail.com", [new EmailNotificationService()]));
 
+        var sprint = new ReviewSprint("Sprint 1", DateTime.Now, DateTime.Now.AddDays(30),
+            new User("Alex Jones", "a.jones@mail.com", [new EmailNotificationService()]));
+
+        Project.AddSprint(sprint);
+
+        var backlogItem = new BacklogItem("Create a new thread");
+
+        sprint.AddBacklogItem(backlogItem);
+
+        StringWriter sw = new StringWriter();
+        Console.SetOut(sw);
+
+        Project.Pull();
+
+        Assert.Contains("Pulling changes from the repository.", sw.ToString());
+        
+        // Reset the console output
+        Console.SetOut(new StreamWriter(Console.OpenStandardOutput()));
+    }
+    
+    [Fact]
+    public void ProjectCanMerge()
+    {
+        var Project = new Project("Project 1",
+            new User("John Doe", "j.doe@mail.com", [new EmailNotificationService()]));
+
+        var sprint = new ReviewSprint("Sprint 1", DateTime.Now, DateTime.Now.AddDays(30),
+            new User("Alex Jones", "a.jones@mail.com", [new EmailNotificationService()]));
+
+        Project.AddSprint(sprint);
+
+        var backlogItem = new BacklogItem("Create a new thread");
+
+        sprint.AddBacklogItem(backlogItem);
+
+        StringWriter sw = new StringWriter();
+        Console.SetOut(sw);
+
+        Project.Merge();
+
+        Assert.Contains("Merging changes from the repository.", sw.ToString());
+        
+        // Reset the console output
+        Console.SetOut(new StreamWriter(Console.OpenStandardOutput()));
+    }
+    
+    [Fact]
+    public void ProjectCanCreateBranch()
+    {
+        var Project = new Project("Project 1",
+            new User("John Doe", "j.doe@mail.com", [new EmailNotificationService()]));
+
+        var sprint = new ReviewSprint("Sprint 1", DateTime.Now, DateTime.Now.AddDays(30),
+            new User("Alex Jones", "a.jones@mail.com", [new EmailNotificationService()]));
+
+        Project.AddSprint(sprint);
+
+        var backlogItem = new BacklogItem("Create a new thread");
+
+        sprint.AddBacklogItem(backlogItem);
+
+        StringWriter sw = new StringWriter();
+        Console.SetOut(sw);
+
+        Project.Branch();
+
+        Assert.Contains("Creating a new branch in the repository.", sw.ToString());
+        
+        // Reset the console output
+        Console.SetOut(new StreamWriter(Console.OpenStandardOutput()));
+    }
 }
