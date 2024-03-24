e19d4fb5c94d8737d7511f47ac08837439eba774
Rogier van der Gaag <rm.vandergaag@student.avans.nl>
2024-03-24 15:04:01 +0100

    Added extra project tests

--- Tests/ProjectTest.cs
+++ Tests/ProjectTest.cs
@@ -29,6 +29,20 @@ public class ProjectTest
     }
 
     [Fact]
+    public void ProjectHasVersionControlSystem()
+    {
+        var Project = new Project("Project 1",
+            new User("John Doe", "j.doe@mail.com", [new EmailNotificationService()]));
+
+        var sprint = new ReviewSprint("Sprint 1", DateTime.Now, DateTime.Now.AddDays(30),
+            new User("Alex Jones", "a.jones@mail.com", [new EmailNotificationService()]));
+        
+        Project.AddSprint(sprint);
+        
+        Assert.NotNull(Project.GitAdapter);
+    }
+
+    [Fact]
     public void ProjectCanCommit()
     {
         var Project = new Project("Project 1",
@@ -55,5 +69,33 @@ public class ProjectTest
         // Reset the console output
         Console.SetOut(new StreamWriter(Console.OpenStandardOutput()));
     }
-    
+
+    [Fact]
+    public void ProjectCanPush()
+    {
+        var Project = new Project("Project 1",
+            new User("John Doe", "j.doe@mail.com", [new EmailNotificationService()]));
+
+        var sprint = new ReviewSprint("Sprint 1", DateTime.Now, DateTime.Now.AddDays(30),
+            new User("Alex Jones", "a.jones@mail.com", [new EmailNotificationService()]));
+
+        Project.AddSprint(sprint);
+
+        var backlogItem = new BacklogItem("Create a new thread");
+
+        sprint.AddBacklogItem(backlogItem);
+
+        using (StringWriter sw = new StringWriter())
+        {
+            Console.SetOut(sw);
+
+            Project.Push();
+
+            Assert.Contains("Pushing changes to the repository.", sw.ToString());
+        }
+
+        // Reset the console output
+        Console.SetOut(new StreamWriter(Console.OpenStandardOutput()));
+    }
+
 }
